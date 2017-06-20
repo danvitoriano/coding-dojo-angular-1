74f2c47599e31000f5176d04f0a2a1d2a17a92a2
Danilo Vitoriano <danilo.vitoriano@oi.net.br>
2017-06-20 16:00:30 -0300

    dojo parte 2 - until part6, item 2

--- public/js/controllers/FotosController.js
+++ public/js/controllers/FotosController.js
@@ -1,18 +1,20 @@
 angular.module("alurapic")
 
-       .controller('FotosController', function($scope) 
+       .controller('FotosController', function($scope, $http) 
 	       {
-		   	  $scope.fotos = [{
-				titulo : 'Leão',
-				url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-			  },
-			  {
-				titulo : 'Leão',
-				url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-			  },
-			  {
-				titulo : 'Leão',
-				url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-			  }]
+	       	$scope.fotos = [];
+	       	$scope.filtro = '';
+
+	       	$http.get(
+	       		'v1/fotos'
+	       		).success(
+	       			function(fotos){
+	       				$scope.fotos = fotos;
+	       			}
+	       		).error(
+	       			function(error){
+	       				console.log(error);
+	       		 	}
+	       		);
 	       }
        );
